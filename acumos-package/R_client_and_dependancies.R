9c70b73a5da2869dff4ecb8dbdc66e9b01f068bd
PhilippDo <philippe.dooze@orange.com>
2020-06-24 11:35:16 +0200

    move code from github to gerrit
    Issue-Id: ACUMOS-4217
    Change-Id: Ide370006b989110ef215c2e951d2962b01aeb94f
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>

---
+++ acumos-package/R_client_and_dependancies.R
@@ -0,0 +1,2 @@
+install.packages(c("httr", "jsonlite","RProtoBuf", "Rserve"),repos='http://cran.r-project.org')
+install.packages("https://github.com/sambaala/R-acumos/archive/v0.3-0.tar.gz", repos=NULL)
